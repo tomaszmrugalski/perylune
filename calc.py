fb1f60d1623f4033ca403cef04ef3884d376f28c
Tomek Mrugalski <tomasz@isc.org>
2019-04-03 22:18:42 +0200

    First GUI skeleton added

---
+++ calc.py
@@ -0,0 +1,58 @@
+#
+
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QVBoxLayout,  QTabWidget, QPushButton
+
+# from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton, QWidget, QAction, QTabWidget,QVBoxLayout
+# from PyQt5.QtGui import QIcon
+# from PyQt5.QtCore import pyqtSlot
+
+class CalcGUI(QMainWindow):
+
+    def __init__(self, argv):
+        super().__init__()
+        self.title = 'OrbCalc'
+        self.left = 0
+        self.top = 0
+        self.width = 1024
+        self.height = 768
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+
+        self.table_widget = CalcGUITabs(self)
+        self.setCentralWidget(self.table_widget)
+ 
+    def run(self):
+
+        self.show()
+
+class CalcGUITabs(QWidget):
+    def __init__(self, parent):
+        super(QWidget, self).__init__(parent)
+        self.layout = QVBoxLayout(self)
+        print("####\n")
+
+        self.tabs = QTabWidget()
+        self.tab1 = QWidget()
+        self.tab2 = QWidget()
+
+        # Add tabs
+        self.tabs.addTab(self.tab1, "Spherical distance")
+        self.tabs.addTab(self.tab2, "RA/Dec")
+
+        print("#### 1\n")
+        self.tab1.layout = QVBoxLayout(self)
+        print("#### 2\n")
+        self.pushButton1 = QPushButton("PyQt5 button")
+        self.tab1.layout.addWidget(self.pushButton1)
+        self.tab1.setLayout(self.tab1.layout)
+
+        self.layout.addWidget(self.tabs)
+        self.setLayout(self.layout)
+
+if __name__ == '__main__':
+
+    app = QApplication(sys.argv)
+    c = CalcGUI(sys.argv)
+    c.run()
+    sys.exit(app.exec_())
