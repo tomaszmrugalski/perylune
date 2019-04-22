31b8b600cf4f36da50387e3ea214830e76370662
Tomek Mrugalski <tomasz@isc.org>
2019-04-22 12:40:56 +0200

    Obliczanie azymutu dziala.

--- gps-lab2.py
+++ gps-lab2.py
@@ -2,6 +2,7 @@
 
 from OrbCalc import *
 import numpy as np
+from math import *
 
 class CoordsECEF:
     x = 0.0
@@ -33,11 +34,11 @@ class GpsDop:
                 # skip comments (starting with one of # ; %)
                 if line[0] in ['#', ';', '%']:
                     continue
-                
+
                 name, x, y, z = line.split(',')
 
                 coords = CoordsECEF(name, x, y, z)
-                
+
                 self.sats.append(coords)
 
         except IOError:
@@ -70,7 +71,7 @@ class GpsDop:
         return np.array([x, y, z])
 
     def getObserverLLA(self, B = 0.0, L = 0.0, h = 0.0):
-        '''Returns observer coords in LLA system. B (topocentric latitude) and 
+        '''Returns observer coords in LLA system. B (topocentric latitude) and
            L (topocentric longitute) are expressed in radians, h expressed in meters
            above spheroid.'''
         return np.array([B, L, h])
@@ -95,7 +96,7 @@ class GpsDop:
         xENU = np.zeros(obj_ecef[0].shape)
         yENU = np.zeros(obj_ecef[0].shape)
         zENU = np.zeros(obj_ecef[0].shape)
-        
+
         for i in range(0, obj_ecef[0].shape[0]):
             tmp = np.array([ vec_x[i], vec_y[i], vec_z[i]])
             result = np.dot(F,tmp)
@@ -104,6 +105,40 @@ class GpsDop:
             zENU[i] = result[2]
 
         return xENU, yENU, zENU
+
+    def calculateAzimuth(self, xENU, yENU):
+
+        AZ = np.zeros(xENU.shape[0])
+
+        for i in range(0, xENU.shape[0]):
+            if xENU[i] == 0 and yENU[i] > 0:
+                AZ[i] = 0
+
+            elif xENU[i] > 0 and yENU[i] > 0:
+                AZ[i] = atan(abs(xENU[i]/yENU[i]))
+
+            elif xENU[i] > 0 and yENU[i] == 0:
+                AZ[i] = pi/2
+
+            elif xENU[i] > 0 and yENU[i] < 0:
+                AZ[i] = pi/2 + atan(abs(yENU[i]/xENU[i]))
+
+            elif xENU[i] == 0 and yENU[i] < 0:
+                AZ[i] = pi
+
+            elif xENU[i] < 0 and yENU[i] < 0:
+                AZ[i] = pi + atan(abs(xENU[i]/yENU[i]))
+
+            elif xENU[i] < 0 and yENU[i] == 0:
+                AZ[i] = 3*pi/2
+
+            elif xENU[i] < 0 and yENU[i] > 0:
+                AZ[i] = 3*pi/2 + atan(abs(yENU[i]/xENU[i]))
+            else:
+                raise Exception("Can't calculate azimuth: i=%d xENU[%d]=%f yENU[%d]=%f" % (i, i, xENU[i], i, yENU[i]))
+
+        return AZ
+
 
 gps = GpsDop()
 
@@ -120,7 +155,9 @@ print("xENU=%s" % xENU)
 print("yENU=%s" % yENU)
 print("zENU=%s" % zENU)
 
+AZ = gps.calculateAzimuth(xENU, yENU)
 
+print(AZ)
 
 
 #print("--------------")
