6612d136f764b1f6787c0ba39eea70e50c91d0cc
Tomek Mrugalski <tomasz@isc.org>
2019-04-22 12:22:08 +0200

    Projekt z mechatroniki

--- smog.py
+++ smog.py
@@ -2,14 +2,22 @@
 
 from OrbCalc import *
 
-a = 35786000 + OrbCalc.getConst('earth-radius')
+# GEO = 35786
+# 880km = 14 orbit/dobe
+# 554,25km = 15 orbit/dobe
+a = 880000 + OrbCalc.getConst('earth-radius')
 
 G = OrbCalc.getConst('G')
 M = OrbCalc.getConst('M')
 
-print("Okres dla orbity kolowej %f [km] wynosi %f [s]" % (a/1000, OrbCalc.getPeriod(a)))
+T = OrbCalc.getPeriod(a)
+print("Okres dla orbity kolowej %f [km] wynosi %f [s]" % (a/1000, T))
+
+day = OrbCalc.getConst('sidereal-day')
 
 print("Dlugosc doby gwiazdowej = %f [s]" % OrbCalc.getConst('sidereal-day'))
+
+print("%f orbit/dobe" % (day/T))
 
 print("Predkosc ucieczki dla Ziemi = %f [m/s]" % OrbCalc.escapeVel())
 
