b9d4e2cc5329d4eae5969ea0425c68ebadcd5c48
Tomek Mrugalski <thomson@klub.com.pl>
2019-09-22 20:03:17 +0200

    Orbit.py now uses a instead of a_sqrt

--- perylune/Orbit.py
+++ perylune/Orbit.py
@@ -5,8 +5,18 @@ class Orbit:
     id = 0
 
     def __init__(self, name):
-        self.name = name
+        self.name = name        # Name of the object
+        self.id = 0             # Id of an object
+
+        self.e = 0.0            # Orbit eccentricity (0-inf), 0=circular, 0..1 - eliptical, 1=parabolic, >1 for hyperbolic.
+        self.a = 0.0            # Major semi-axis
+        self.incl = 0.0         # Orbit Inclination (degrees 0-359.9)
+        self.toa = 0.0          #
+        self.ra_rate = 0.0      #
+        self.ra_week = 0        #
+        self.aop = 0            #
+        self.mean_anomaly = 0.0 #
 
     def getText(self):
         return "%s: id=%s eccentricity=%f toa=%f incl=%f rate-of-ra=%8e sqrt(a)=%f ra-at-week=%f aop=%f mean-anom=%f" % \
-        (self.name, self.id, self.e, self.toa, self.incl, self.ra_rate, self.a_sqrt, self.ra_week, self.aop, self.mean_anomaly)
+        (self.name, self.id, self.e, self.toa, self.incl, self.ra_rate, self.a, self.ra_week, self.aop, self.mean_anomaly)
