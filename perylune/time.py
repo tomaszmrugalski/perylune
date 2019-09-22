07a166f03a848ed17063c261978b773454b71432
Tomek Mrugalski <thomson@klub.com.pl>
2019-09-22 20:02:29 +0200

    time added
    Basic time operations.

---
+++ perylune/time.py
@@ -0,0 +1,47 @@
+# Perylune
+# by Tomek Mrugalski
+
+# Source: https://stjarnhimlen.se/comp/ppcomp.html
+
+def leap_year(y):
+    if not y%400:
+        return True
+    if not y%100:
+        return False
+    if y%4:
+        return False
+    else:
+        return True
+
+
+def leap_years(min, max):
+    """Returns number of leap years between year X and Y"""
+    if min > max:
+        min,max = max,min
+
+    leap = 0
+    for i in range(min,max):
+        if leap_year(i):
+            leap += 1
+
+    return leap
+
+def days(y, m, d, ut = 0.0):
+    """Calculates number of days since 2000 Jan 1 0.0 UT"""
+    """y - year, e.g. 2019"""
+    """m - month (1-12), e.g. 9"""
+    """d - day of the month (1-31), e.g. 22"""
+    """ut - universal time, 0.0... 23.99999 - expressed in hours, minutes and seconds expressed as fraction of hour """
+
+    # Simple equation valid for March 1900 - Feb 2100.
+    days = d = 367*y - 7 * int(( y + int((m+9)/12) ) / 4) + int(275*m/9) + d - 730530
+
+    # More complex formula valid over the entire Gregorian Calendar.
+    #days = 367*y - 7 * int(( y + int((m+9)/12) ) / 4) - 3 * ( int(( y + int((m-9)/7) ) / 100 + 1 )) / 4 + int(275*m/9) + d - 730515
+
+    days = days + ut/24.0
+
+    return days
+
+
+
---
+++ tests/time_test.py
@@ -0,0 +1,35 @@
+from perylune import time
+
+def test_leap_year():
+    # Tests if certain years are properly detected as leap years.
+    values = [ [2000, True], [2019, False], [2001, False], [2004, True], [1900, False], [2100, False]]
+
+    for i,row in enumerate(values):
+        assert time.leap_year(row[0]) == row[1]
+
+def test_leap_years():
+    values = [ [2000, 2000, 0], [2000, 2001, 1], [2000, 2010, 3], [2000, 2100, 25] ]
+
+    for i,row in enumerate(values):
+        assert time.leap_years(row[0], row[1]) == row[2]
+
+def test_days_0():
+    days = time.days(1999,12,31,0.0)
+    assert days == 0.0
+
+def test_days_2019_09_22():
+    days = time.days(2019, 9, 22, 0)
+    expected = 19*365 + 5 # 19 years, five leap years (2000, 2004, 2008, 2012, 2016)
+    expected += 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 # Full months since beginning of the year.
+    expected += 21 # 21 full days since beginning of the year.
+    expected += 1 # Because 2000-Jan-1 was day number 1
+    assert days == expected
+
+def test_days_2019_09_22_15h55m():
+    days = time.days(2019, 9, 22, 15 + (55/60))
+    expected = 19*365 + 5 # 19 years, five leap years (2000, 2004, 2008, 2012, 2016)
+    expected += 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 # Full months since beggining of the year.
+    expected += 21 # 21 full days since beginning of the year.
+    expected += 1 # Because 2000-Jan-1 was day number 1
+    expected += 15/24 + 55/(24*60) # 15h 55mins
+    assert days == expected
