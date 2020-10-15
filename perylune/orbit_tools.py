25e1d154f9e8e250020df69da9fc77135f966676
Tomek Mrugalski <tomasz@isc.org>
2020-10-15 22:28:03 +0200

    inclination change chart fixed

--- perylune/orbit_tools.py
+++ perylune/orbit_tools.py
@@ -11,10 +11,10 @@ def print_orb(o: Orbit):
         b = a*sqrt(1-e*e)
     else:
         b = 0 * u.km
-    a = a.to(u.km)
-    b = b.to(u.km)
-    apo = o.r_a.to(u.km)
-    per = o.r_p.to(u.km)
+    a = a.to(u.m) / 1000.0
+    b = b.to(u.m) / 1000.0
+    apo = o.r_a.to(u.m) / 1000.0
+    per = o.r_p.to(u.m) / 1000.0
     print("𝑎=%4.2f%s, b=%4.2f%s, e=%4.2f%s, i=%4.2f%s raan(Ω)=%4.2f%s argp(𝜔)=%4.2f%s nu(𝜈)=%4.2f%s period=%s perapis=%s apoapsis=%s" % \
         (a.value, a.unit, b.value, b.unit, e.value, e.unit, i.value, i.unit, raan.value, raan.unit, argp.value, argp.unit, nu.value, nu.unit, o.period, per, apo))
 
@@ -28,11 +28,13 @@ def inc_change(o1: Orbit, o2: Orbit):
         Retuns value in m/s (the returned value is using units)
     """
     di = o2.inc - o1.inc  # delta-i (the difference in inclination)
-    a = o1.a # semi-major axis (in meters)
+    a = o1.a.to(u.km) # semi-major axis (in meters)
     e = o1.ecc # eccentricity dimensionless
     argp = o1.argp # argument of perigeum (radians)
     nu = o1.nu # radians
     n = o1.n / u.rad # mean motion (let)
+
+    #print("inc_change: n=%s a=%s n*a=%s (1 + ecos(nu)=%s" % (repr(n), repr(a), repr(n*a), repr(1 + e*np.cos(nu)) ))
 
     delta_v = 2*np.sin(di/2.0)*sqrt(1-e*e)*np.cos(argp + nu) * n * a / (1 + e*np.cos(nu))
 
