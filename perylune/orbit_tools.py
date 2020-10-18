fixed d-v required for inclination calculation

perylune/orbit_tools.py
@@ -35,17 +35,20 @@ def inc_change(o1: Orbit, o2: Orbit):
     """
         Return delta-v cost required for pure inclination change manouver between two orbits.
         Retuns value in m/s (the returned value is using units)
+
+        Reference: https://en.wikipedia.org/wiki/Orbital_inclination_change#Calculation
+
+        Note: The equation on wikipedia is slightly broken. The cost(arg of periapsis + true anomaly) should be inside the
+        square root.
     """
-    di = o2.inc - o1.inc  # delta-i (the difference in inclination)
-    a = o1.a.to(u.km) # semi-major axis (in meters)
+    di = o2.inc - o1.inc  # delta-i (the difference in inclination, in degrees or radians)
+    a = o1.a.to(u.km) # semi-major axis (in kilometers)
     e = o1.ecc # eccentricity dimensionless
     argp = o1.argp # argument of perigeum (radians)
-    nu = o1.nu # radians
-    n = o1.n / u.rad # mean motion (let)
+    nu = o1.nu # true anomaly (nu), in radians
+    n = o1.n / u.rad # mean motion (rad/s)
 
-    #print("inc_change: n=%s a=%s n*a=%s (1 + ecos(nu)=%s" % (repr(n), repr(a), repr(n*a), repr(1 + e*np.cos(nu)) ))
-
-    delta_v = 2*np.sin(di/2.0)*sqrt(1-e*e)*np.cos(argp + nu) * n * a / (1 + e*np.cos(nu))
+    delta_v = 2*np.sin(di/2.0)*sqrt(1-e*e*np.cos(argp + nu)) * n * a / (1 + e*np.cos(nu))
 
     return delta_v
 
@@ -69,7 +72,8 @@ def calc_vel(o: Orbit, delta_t: u, n: int):
 def calc_vel_inc_cost(o1: Orbit, o2: Orbit, delta_t: u, n: int):
     """
         Calculates velocity for orbit o in n intervals, each lasting t period.
-        Returned array has v(scalar), t(time)
+        Returned array has v(scalar), t(time), nu(), argument of perigeum, delta-v of incination change, r (distance from
+        attractor center)
     """
     vel = []
     orb = o1
@@ -78,7 +82,8 @@ def calc_vel_inc_cost(o1: Orbit, o2: Orbit, delta_t: u, n: int):
         v_scalar = np.sqrt(v[0]**2 + v[1]**2 + v[2]**2)
         t = orb.epoch
         inc_delta_v = inc_change(orb, o2)
-        vel.append([v_scalar, t, orb.nu, orb.argp, inc_delta_v])
+        r = np.linalg.norm(orb.r) # Distance from Earth center
+        vel.append([v_scalar, t, orb.nu, orb.argp, inc_delta_v, r])
         orb = orb.propagate(delta_t)
 
     return vel