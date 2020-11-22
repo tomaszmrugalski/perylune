8e8734322ee898d483d91216806dc8600147e848
Tomek Mrugalski <thomson@klub.com.pl>
2020-11-22 18:39:24 +0100

    Const acceleration perturber is ready

---
+++ perylune/perturbers.py
@@ -0,0 +1,37 @@
+
+# This file contains several perturbers, each modelling different phenomena.
+#
+# You can use those perturbers by passing them as ad parameter to propagation, for example:
+
+# times = np.linspace(0, 100 * orb.period, 1000)
+# positions = propagate(orb, time.TimeDelta(times), method=cowell, rtol=1e-11, ad=pert_constant_accel(accel) )
+#
+# or:
+#
+# orb.propagate(tof, method=cowell, ad=pert_solar_sail(accel), rtol=1e-11)
+
+# Perturber 1: This is a constant forward acceleration. This could model an ion engine,
+# but it's very simplified. This more like a theoretical model that can be used to prove that
+# the perturber really works.
+#
+# Based on poliastro example.
+def constant_accel(accel):
+
+    # TODO: This is messed up. The parameter passed here (accel) is not used anywhere. Instead, the
+    # _const_accel is used instead.
+    _const_accel = accel # 3e-5 # expressed in km/s
+
+    def constant_acc(t0, u, k):
+        """ This function is called every time the propagation algorithm needs to determine the perturbation.
+
+        t0 - time in seconds since beginning
+        u - state vectors, u[0:2] is r (position), u[3:5] - velocity
+        k - GM constant, expressed in km3/s2 (so it's 398600.4418 for Earth)
+        return 3 values - x,y,z delta (in km)
+        """
+
+        v = u[3:]
+        norm_v = (v[0]**2 + v[1]**2 + v[2]**2)**.5 # expressed in km/s
+        return _const_accel * v / norm_v
+
+    return constant_acc
