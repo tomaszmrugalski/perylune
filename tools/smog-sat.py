815f3992a5aef94bcfc77c23cd06530f17b5af4a
Tomek Mrugalski <thomson@klub.com.pl>
2020-11-25 12:26:12 +0100

    Updated old, old calculations

--- tools/smog-sat.py
+++ tools/smog-sat.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 
-from perylune.OrbCalc import *
+from perylune.orbcalc import OrbCalc
 
-# GEO = 35786
+# GEO = 35786 km
 # 880km = 14 orbit/dobe
 # 554,25km = 15 orbit/dobe
 
-orb = OrbCalc()
+print("=== orbit 1: LEO (880km) ===")
 
 a = 880000 + OrbCalc.getConst('earth-radius')
 
@@ -14,16 +14,22 @@ G = OrbCalc.getConst('G')
 M = OrbCalc.getConst('M')
 
 T = OrbCalc.getPeriod(a)
-print("Okres dla orbity kolowej %f [km] wynosi %f [s]" % (a/1000, T))
+print("Period of a circular orbit of radius %f [km] is %f [s]" % (a/1000, T))
 
 day = OrbCalc.getConst('sidereal-day')
 
-print("Dlugosc doby gwiazdowej = %f [s]" % OrbCalc.getConst('sidereal-day'))
+print("Sidereal day (dlugosc doby gwiazdowej) = %f [s]" % OrbCalc.getConst('sidereal-day'))
 
-print("Predkosc ucieczki dla Ziemi = %f [m/s]" % OrbCalc.escapeVel())
+print("Escape velocity for Earth (predkosc ucieczki dla Ziemi) = %f [m/s]" % OrbCalc.escapeVel())
+
+print("=== orbit 2: GEO (35786) ===")
 
 a = 42164 * 1000
 T = OrbCalc.getPeriod(a)
-print("Okres dla orbity kolowej %f [km] wynosi %f [s]" % (a/1000, T))
+print("Period of a circular orbit of radius %f [km] is %f [s]" % (a/1000, T))
 
-print("%f orbit/dobe" % (day/T))
+print("%f orbits/sidereal day" % (day/T))
+
+alt = a - OrbCalc.getConst('earth-radius')
+
+print("GEO altitude (Wysokosc orbity GEO) is %f [km]" % (alt / 1000.0))
