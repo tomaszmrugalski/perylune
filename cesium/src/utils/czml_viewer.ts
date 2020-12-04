208d87e4441c7193746f6369c4d5157160b01f1a
Tomek Mrugalski <thomson@klub.com.pl>
2020-12-04 19:40:09 +0100

    List of sats added, tracking sats updated.

--- cesium/src/utils/czml_viewer.ts
+++ cesium/src/utils/czml_viewer.ts
@@ -9,6 +9,22 @@
 
 import * as Cesium from "cesium";
 
+let sats : Cesium.Entity[] = [];
+
+function addSat(sat: Cesium.Entity) {
+    sats.push(sat);
+}
+
+function getSat(sat_id: string) : Cesium.Entity | undefined  {
+
+    console.log(sats.length + " sat(s) defined.");
+    let s = sats.find(sat => sat.id == sat_id);
+    console.log("getSat(" + sat_id + ") returns " + s);
+
+
+    return s;
+}
+
 export function czmlViewer(viewer: Cesium.Viewer) {
 
     // This updates constellation buttons
@@ -31,5 +47,106 @@ export function czmlViewer(viewer: Cesium.Viewer) {
 function loadCzml(viewer: Cesium.Viewer, czml: string) {
     var czmlDataSource = new Cesium.CzmlDataSource();
     viewer.dataSources.add(czmlDataSource);
-    czmlDataSource.load(czml);
+
+    // We should load the DataSource. The load() function returns a promise, which will eventually resolve
+    // once the file is actually loaded. It will trigger the function specifide by then().
+    czmlDataSource.load(czml).then(function(){
+        updateUI(viewer);
+        updateButtonCallbacks(viewer);
+
+        console.log("Loaded " + sats.length + " sat(s).");
+    });
+
+}
+
+// This function is called when the CZML data source finished loading.
+function updateUI(viewer:Cesium.Viewer) {
+    const dses = viewer.dataSources;
+    if (dses.length < 1) {
+        console.log("ERROR: No datasources loaded.");
+        return;
+    }
+
+    const tableBody = document.querySelector('#satList tbody') as HTMLTableSectionElement;
+
+    const ds = dses.get(0); // DataSource
+    var entities = ds.entities.values; // Cesium.EntityCollection col;// = ds.entities;
+    // ds.entities = Cesium.EntityCollection.
+    console.log(entities);
+    for (var i =0; i < entities.length; i++) {
+        var e : Cesium.Entity = entities[i];
+
+        var props = e.properties;
+
+        console.log(i + ": id=" + e.id + " name=" + e.name + ", TLE=" + props);
+        if (e.name === undefined) {
+            continue;
+        }
+
+        addSat(e);
+
+        const row = tableBody.insertRow();
+        row.id = 'info-' + e.name;
+        row.dataset['sat_id'] = e.id;
+        row.dataset['sat_name'] = e.name;
+
+        // cell 1: Sat button
+        let cell = row.insertCell();
+        let btn = document.createElement('button');
+        btn.classList.add('cesium-button', 'button-sat-focus');
+        btn.dataset['sat_id'] = e.id;
+        btn.dataset['sat_name'] = e.name;
+        btn.innerText = e.name;
+        cell.appendChild(btn);
+
+        let cell2 = row.insertCell();
+        cell2.innerText = "TAK";
+
+        let cell3 = row.insertCell();
+        let btn2 = document.createElement('button');
+        btn2.classList.add('cesium-button', 'button-sat-path');
+        btn2.dataset['sat_id'] = e.id;
+        btn2.dataset['sat_name'] = e.name;
+        btn2.innerText = "Widać";
+        cell3.appendChild(btn2);
+    }
+
+}
+
+function updateButtonCallbacks(viewer: Cesium.Viewer) {
+
+    // This updates Sat buttons
+    const flyToAirportButtons = [...document.querySelectorAll(".button-sat-focus")] as HTMLButtonElement[];
+    flyToAirportButtons.forEach(btn => {
+        const sat_id = btn.dataset["sat_id"];
+        if (sat_id === undefined) {
+            return;
+        }
+        btn.onclick = () => focusCameraTo(viewer, sat_id);
+    });
+
+    // This updates Sat trajectory visibility buttons
+    const toggleSatPathButtons = [...document.querySelectorAll(".button-sat-path")] as HTMLButtonElement[];
+    toggleSatPathButtons.forEach(btn => {
+        const sat_id = btn.dataset["sat_id"];
+        if (sat_id === undefined) {
+            return;
+        }
+        btn.onclick = () => toggleSatPath(viewer, sat_id);
+    });
+
+}
+
+function focusCameraTo(viewer: Cesium.Viewer, sat_id: string) {
+    console.log("focusCameraTo(.., " + sat_id + ") clicked");
+    var x = getSat(sat_id);
+    if (viewer.trackedEntity == x) {
+        viewer.trackedEntity = undefined; //
+    } else {
+        viewer.trackedEntity = x;   //
+    }
+}
+
+function toggleSatPath(viewer: Cesium.Viewer, sat_id: string) {
+    console.log("toggleSatPath(.., " + sat_id + ") clicked");
 }
